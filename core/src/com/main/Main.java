f3cd8ad948481e192573bfec5109f61c043dd67c
kyla-klov <132112944+kyla-klov@users.noreply.github.com>
2024-03-01 13:59:00 +0000

    created MainGameScreen

--- core/src/com/main/Main.java
+++ core/src/com/main/Main.java
@@ -1,50 +1,31 @@
 package com.main;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.main.screens.MainGameScreen;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	float x;
-	float y;
-	
+import static com.badlogic.gdx.Gdx.graphics;
+
+public class Main extends Game {
+	/* this is for movement speed so that it can use delta time to keep
+	* the frame movement constant so matter the frames that are set
+	*/
+	public SpriteBatch batch;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new MainGameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-
-		if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
-			y += 4;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
-			y -= 4;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
-			x -= 4;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
-			x += 4;
-		}
-
-
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/main/screens/MainGameScreen.java
@@ -0,0 +1,77 @@
+package com.main.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.main.Main;
+
+public class MainGameScreen implements Screen {
+
+    public static final float speed = 120;
+
+    Texture img;
+    float x;
+    float y;
+
+    Main game;
+
+    public MainGameScreen (Main Game) {
+    }
+
+    @Override
+    public void show () {
+        img = new Texture("badlogic.jpg");
+        game.batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render (float delta) {
+        // MOVEMENT WITH DELTA TIME
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
+            y += (speed * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
+            y -= (speed * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
+            x -= (speed * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
+            x += (speed * Gdx.graphics.getDeltaTime());
+        }
+
+        ScreenUtils.clear(0, 0, 0, 1);
+        game.batch.begin();
+        game.batch.draw(img, x, y);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize (int width, int height) {
+
+    }
+
+    @Override
+    public void pause () {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        game.batch.dispose();
+        img.dispose();
+    }
+}
