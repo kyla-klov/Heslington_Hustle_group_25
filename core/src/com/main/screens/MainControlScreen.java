58e292c520bcc1be9f79cc951fd5677d9dfbe9d5
mgdevelopment00 <135275238+mgdevelopment00@users.noreply.github.com>
2024-03-05 13:24:05 +0000

    MainControlsClass
    added some stuff

---
+++ core/src/com/main/screens/MainControlScreen.java
@@ -0,0 +1,63 @@
+package com.main.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainControlScreen implements Screen {
+
+    private Map<String, String> controlMapping = new HashMap<String, String>();
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 1, 1);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    public void addControls(){
+        addControl("W", "Up");
+        addControl("A", "Left");
+        addControl("S", "Right");
+        addControl("D", "Down");
+    }
+
+    public void addControl(String key, String action) throws IllegalArgumentException{
+        if (controlMapping.containsKey(key)){
+            throw new IllegalArgumentException("binding already exists");
+        } else {
+            controlMapping.put(key, action);
+        }
+    }
+
+}
