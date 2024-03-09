b1334db83d454620b38384a65411f9cc7c80fdb5
Hannah Thompson <ht1226@york.ac.uk>
2024-03-09 16:02:45 +0000

    added Back button to Controls screen

--- core/src/com/main/screens/MainControlScreen.java
+++ core/src/com/main/screens/MainControlScreen.java
@@ -1,29 +1,98 @@
 package com.main.screens;
-
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.main.Main;
-
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainControlScreen implements Screen {
+import static com.badlogic.gdx.Gdx.input;
+
+public class MainControlScreen implements Screen, InputProcessor {
 
     Main game;
     private final Map<String, String> controlMapping = new HashMap<String, String>();
 
+    private Texture backButtonTexture;
+    private float backButtonX;
+    private float backButtonY;
+    private final float backButtonWidth = 100;
+    private final float backButtonHeight = 50;
+
     public MainControlScreen(Main game) {
         this.game = game;
+        backButtonTexture = new Texture("assets/menu_buttons/back_button.png");
+        backButtonX = 10; // Adjust the position as needed
+        backButtonY = game.screenHeight - backButtonHeight - 10;
     }
 
     @Override
     public void show() {
-
+        input.setInputProcessor(this);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 1, 1);
+        game.batch.begin();
+        game.batch.draw(backButtonTexture, backButtonX, backButtonY, backButtonWidth, backButtonHeight);
+        game.batch.end();
+    }
+
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        float worldX = screenX * game.screenWidth / (float) Gdx.graphics.getWidth();
+        float worldY = game.screenHeight - screenY * game.screenHeight / (float) Gdx.graphics.getHeight();
+
+        if (worldX >= backButtonX && worldX <= backButtonX + backButtonWidth &&
+                worldY >= backButtonY && worldY <= backButtonY + backButtonHeight) {
+            game.setScreen(new MainMenuScreen(game));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        // Implement scrolling behavior if needed
+        return false; // Return false if the event was not handled
     }
 
     @Override
@@ -48,6 +117,7 @@ public class MainControlScreen implements Screen {
 
     @Override
     public void dispose() {
+        backButtonTexture.dispose();
 
     }
 
