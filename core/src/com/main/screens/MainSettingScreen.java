9673089b2a20d898c60f8397cdedc4a8a0b41d24
mgdevelopment00 <135275238+mgdevelopment00@users.noreply.github.com>
2024-03-09 16:17:32 +0000

    Create MainSettingScreen.java

---
+++ core/src/com/main/screens/MainSettingScreen.java
@@ -0,0 +1,41 @@
+package com.main.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainSettingScreen implements Screen {
+    @Override
+    public void show() {
+        ScreenUtils.clear(0.3f, 0.55f, 0.7f, 1);
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
